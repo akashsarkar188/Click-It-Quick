73db5559c6e629c52dde841f04b3bf88af358d2e
upjotsingh28 <upjot.28@gmail.com>
2021-10-21 15:32:19 +0530

    initial commit

---
+++ app/src/main/java/akash/sarkar/clickquick/activities/LevelSelector.java
@@ -0,0 +1,16 @@
+package akash.sarkar.clickquick.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import akash.sarkar.clickquick.R;
+
+public class LevelSelector extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_level_selector);
+    }
+}
